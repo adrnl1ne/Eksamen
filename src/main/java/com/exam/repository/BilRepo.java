b0dcd9869b84b3ce260d6e90fe73f0f57cfbdc46
adrnl1ne <jako7096@stud.kea.dk>
2022-11-29 15:24:58 +0100

    bilmodelrepo og bilrepo update

--- src/main/java/com/exam/repository/BilRepo.java
+++ src/main/java/com/exam/repository/BilRepo.java
@@ -12,54 +12,6 @@ import java.util.ArrayList;
 public class BilRepo {
     private final Connection DCM = com.exam.Utilities.DCM.getConn();
 
-    public void DeleteBil(Bil bil) {
-        String stelnummer = bil.getStelnummer();
-        String Delete_Query = "DELETE FROM bil WHERE Stelnummer=?";
-        try {
-            PreparedStatement preparedStatement = DCM.prepareStatement(Delete_Query);
-
-            preparedStatement.setString(1, stelnummer);
-            preparedStatement.executeUpdate();
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        System.out.println("Car has been deleted, where stelnummer is: " + stelnummer);
-    }
-
-    public void CreateBil(Bil bil) {
-
-        String QUERY = "INSERT INTO bil (Stelnummer, Model_ID, Km_Kørt, Tilstands_ID) VALUES (?,?,?,?)";
-        try {
-            PreparedStatement preparedStatement = DCM.prepareStatement(QUERY);
-            preparedStatement.setString(1, bil.getStelnummer());
-            preparedStatement.setInt(2, bil.getModel().getModel_ID());
-            preparedStatement.setDouble(3, bil.getKm_kørte());
-            preparedStatement.setInt(4, bil.getTilstand().getId());
-            preparedStatement.executeUpdate();
-        } catch (SQLException e) {
-            System.out.println("Can't create car" + bil.toString());
-            e.printStackTrace();
-        }
-    }
-
-    public void getBilByStelnummer(Bil bil, String stel) {
-        String stelnummer = bil.getStelnummer();
-        String QUERY = "SELECT FROM bilmodel WHERE Stelnummer=?";
-
-        try {
-            PreparedStatement preparedStatement = DCM.prepareStatement(QUERY);
-
-            if (stel == stelnummer) {
-                preparedStatement.setString(1, stel);
-                preparedStatement.executeUpdate();
-            }
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        System.out.println(bil.toString());
-    }
 
 
 
--- src/main/java/com/exam/repository/BilmodelRepo.java
+++ src/main/java/com/exam/repository/BilmodelRepo.java
@@ -12,34 +12,6 @@ import java.util.List;
 public class BilmodelRepo {
     private final Connection DCM = com.exam.Utilities.DCM.getConn();
 
-    public void getBilModel(BilModel bilModel, int Model_ID) {
-        int id = bilModel.getModel_ID();
-        String QUERY = "SELECT FROM bilmodel WHERE Model_ID=?";
 
-        try {
-            PreparedStatement preparedStatement = DCM.prepareStatement(QUERY);
-
-            if (Model_ID == id) {
-                preparedStatement.setInt(1, Model_ID);
-                preparedStatement.executeUpdate();
-            }
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        System.out.println(bilModel.toString());
-    }
-
-   /* public List<String> GetAlleBiler(BilModel bilModel) {
-        int id = bilModel.getModel_ID();
-        String GetAll_QUERY = "SELECT * FROM bilmodel WHERE Model_ID=?";
-        try {
-            Statement statement = DCM.createStatement();
-            ResultSet resultSet = statement.executeQuery(GetAll_QUERY);
-            while (resultSet.next())
-            preparedStatement.setInt(1, id);
-        }
-
-    }*/
 
 }
