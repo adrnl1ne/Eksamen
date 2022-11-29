b753258f5ebe3031b1e50d9488e10fb2e46cfab0
adrnl1ne <jako7096@stud.kea.dk>
2022-11-29 15:30:01 +0100

    bilrepo update

--- src/main/java/com/exam/repository/BilRepo.java
+++ src/main/java/com/exam/repository/BilRepo.java
@@ -6,16 +6,41 @@ import com.exam.model.entities.biler.BilModel;
 import org.springframework.stereotype.Repository;
 
 import java.sql.*;
-import java.util.ArrayList;
+
 
 @Repository
 public class BilRepo {
     private final Connection DCM = com.exam.Utilities.DCM.getConn();
+    public void DeleteBil(Bil bil) {
+        String stelnummer = bil.getStelnummer();
+        String Delete_Query = "DELETE FROM bil WHERE Stelnummer=?";
+        try {
+            PreparedStatement preparedStatement = DCM.prepareStatement(Delete_Query);
 
+            preparedStatement.setString(1, stelnummer);
+            preparedStatement.executeUpdate();
 
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Car has been deleted, where stelnummer is: " + stelnummer);
+    }
 
+    public void CreateBil(Bil bil) {
 
-
+        String QUERY = "INSERT INTO bil (Stelnummer, Model_ID, Km_Kørt, Tilstands_ID) VALUES (?,?,?,?)";
+        try {
+            PreparedStatement preparedStatement = DCM.prepareStatement(QUERY);
+            preparedStatement.setString(1, bil.getStelnummer());
+            preparedStatement.setInt(2, bil.getModel().getModel_ID());
+            preparedStatement.setDouble(3, bil.getKm_kørte());
+            preparedStatement.setInt(4, bil.getTilstand().getId());
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println("Can't create car" + bil.toString());
+            e.printStackTrace();
+        }
+    }
 
 }
 
